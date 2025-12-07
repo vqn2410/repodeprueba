f0f58ef9ade6fd81487d950352682be74e1dbc1c
Nicolás Vergara Quaresima <vergaranicolas209@gmail.com>
2025-12-06 21:34:04 -0300

    Cambios JS

---
+++ main.js
@@ -0,0 +1,59 @@
+// VARIABLE STRING
+
+let  Client_Name
+
+Client_Name = "Nicolas"
+
+//VARIABLE NUMBER
+
+let client_number
+
+client_number = 43797089
+
+
+
+//VARIANTE BOOLIANG
+
+let esEstudiante
+
+esEstudiante=true
+
+let esDocente
+
+esDocente=false
+
+let esFamilia
+
+esFamilia=false
+
+;
+
+if (esEstudiante = false) {
+  alert ("Inicio de Sesión exitoso" + " " + Client_Name)
+} else {
+   alert ("Usted ingreso con un usuario de")
+}
+
+
+
+let edad = 24
+
+let acceso = (edad >= 18) && (edad <= 30);
+console.log(acceso); // true si la edad está entre 18 y 30
+
+if (edad >= 18) {
+  alert ("Permiso Concedido" + " " + Client_Name)
+} else {
+   alert ("Usuario restringido")
+}
+
+console.log("Número de Cliente", client_number);
+console.log ("Nombre de Cliente", Client_Name);
+
+console.log("Estudiante",esEstudiante);
+console.log("Docente",esDocente)
+console.log("Familia",esFamilia)
+console.log("Edad",edad)
+
+
+
