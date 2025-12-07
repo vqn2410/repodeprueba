f8b2f9e96534c99c7de26a479f324c9f0548a448
Nicolás Vergara Quaresima <vergaranicolas209@gmail.com>
2025-12-06 22:04:12 -0300

    Actualización JS

--- main.js
+++ main.js
@@ -28,11 +28,11 @@ esFamilia=false
 
 ;
 
-if (esEstudiante = false) {
-  alert ("Inicio de Sesión exitoso" + " " + Client_Name)
-} else {
-   alert ("Usted ingreso con un usuario de")
-}
+//if (esEstudiante = false) {
+//  alert ("Inicio de Sesión exitoso" + " " + Client_Name)
+//} else {
+//   alert ("Usted ingreso con un usuario de")
+//}
 
 
 
@@ -41,10 +41,21 @@ let edad = 24
 let acceso = (edad >= 18) && (edad <= 30);
 console.log(acceso); // true si la edad está entre 18 y 30
 
-if (edad >= 18) {
-  alert ("Permiso Concedido" + " " + Client_Name)
+//if (edad >= 18) {
+  //alert ("Permiso Concedido" + " " + Client_Name)
+//} else {
+  // alert ("Usuario restringido")
+//}
+
+
+let calificacion = 75;
+console.log("Calificación:", calificacion)
+if (calificacion >= 90) {
+    console.log("Excelente");
+} else if (calificacion >= 70) {
+    console.log("Bueno");
 } else {
-   alert ("Usuario restringido")
+    console.log("Necesitas estudiar más");
 }
 
 console.log("Número de Cliente", client_number);
@@ -56,4 +67,10 @@ console.log("Familia",esFamilia)
 console.log("Edad",edad)
 
 
+
+
+let temperatura = 35;
+console.log("Temperatura", temperatura)
+let advertencia = (temperatura < 1) || (temperatura > 32);
+console.log(advertencia); // true si la temperatura es menor que 0 o mayor que 32
 
